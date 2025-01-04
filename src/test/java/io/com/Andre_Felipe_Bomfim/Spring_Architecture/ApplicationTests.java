21dc45907170cef963f49677b69a57086c167cb1
andre <strongerandrebomfim@gmail.com>
2025-01-04 14:44:54 -0300

    Update a git and gitignore

---
+++ src/test/java/io/com/Andre_Felipe_Bomfim/Spring_Architecture/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.com.Andre_Felipe_Bomfim.Spring_Architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
